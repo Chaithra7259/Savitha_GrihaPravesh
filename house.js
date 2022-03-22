172908732799bb41dadf06b2ae32633fe3b43c86
Chaithra7259 <chaithra7259@gmail.com>
2022-03-22 20:51:11 +0100

    updated style

---
+++ house.js
@@ -0,0 +1,5 @@
+debugger;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // set the body class
+    document.getElementsByTagName('body')[0].className+='mobile'
+  }
